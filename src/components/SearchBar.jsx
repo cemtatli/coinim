db873fa4055af7681c11688afa50c3cba5faab19
Cem <cemtatli3535@gmail.com>
2023-03-16 18:43:16 +0300

    fix: search-box icon problem

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,10 +13,10 @@ export default function SearchBar({ coins }) {
           <input
             type="search"
             onChange={(e) => setSearchValue(e.target.value)}
-            className="  mx-auto h-10 w-full  max-w-[1440px] rounded-lg border border-blue-500 px-4 py-2 pl-10 text-sm outline-none placeholder:text-xs focus:ring-2 dark:bg-transparent dark:text-white md:placeholder:text-sm"
+            className="relative  mx-auto h-10 w-full max-w-[1440px] rounded-lg border border-blue-500 px-4 py-2 pl-10 text-sm outline-none placeholder:text-xs focus:ring-2 dark:bg-transparent dark:text-white  md:placeholder:text-sm"
             placeholder="Kripto ara..."
           />{" "}
-          <SearchNormal1 className="absolute  left-2 top-5 ml-2 h-4 w-4 translate-x-1/2  font-semibold  text-blue-500 dark:text-white" />
+          <SearchNormal1 className="absolute left-4 top-5  h-4 w-4 translate-x-1/2 font-semibold text-blue-500  dark:text-white  2xl:left-2 " />
         </form>
       </div>
       {/* Liste */}
