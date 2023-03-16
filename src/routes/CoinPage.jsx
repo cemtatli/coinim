356042c1b79cb0fe43166a6d8da5af6cbf09e827
Cem <cemtatli3535@gmail.com>
2023-03-16 16:36:37 +0300

    feat: coin detail page customize

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 export default function CoinPage() {
   const [coin, setCoin] = useState({});
@@ -17,53 +18,89 @@ export default function CoinPage() {
   return (
     <div className="mx-auto flex w-full max-w-[1440px] flex-col items-center justify-center overflow-hidden px-5 2xl:px-0">
       <div className="mt-5 flex h-full w-full flex-col items-center justify-center gap-4 ">
-        <div className="flex w-full flex-col items-center gap-2 border-b border-white border-opacity-10 pb-5">
+        {/* Coin Photo Symbol ve Adi */}
+        <div className="flex w-full flex-col items-center gap-2 border-b border-blue-500 border-opacity-10 pb-5 dark:border-white dark:border-opacity-10">
           <img src={coin.image?.large} alt={coin.name} className="h-24 w-24 md:h-32 md:w-32" />
           <div className="flex items-center justify-center gap-2">
             <h1 className="text-xl font-bold dark:text-white md:text-2xl">{coin.name}</h1>
-            <p className="text-base font-semibold uppercase text-gray-500 md:text-base">
+            <p className="text-sm font-medium uppercase text-gray-500 md:text-sm">
               ({coin.symbol})
             </p>
           </div>
+          <div className="flex w-full items-center justify-center">
+            <p className="text-xl font-medium dark:text-white">
+              {coin.market_data?.current_price.usd > 0 ? (
+                <p className="  text-green-500 dark:text-green-400">
+                  {coin.market_data?.current_price.usd}$
+                </p>
+              ) : (
+                <p className="text-red-500 dark:text-red-500">
+                  {coin.market_data?.current_price.usd}$
+                </p>
+              )}
+            </p>
+          </div>
         </div>
-        <div className="flex w-full flex-col items-center justify-center md:flex-row md:gap-4">
-          <div className="flex flex-col items-center justify-center gap-2 px-4 py-2">
-            <p className="rounded-full px-2 py-0.5 text-base font-medium text-blue-500 dark:bg-blue-100 dark:text-slate-900 md:text-base">
-              Current Price
-            </p>
-            <p className="text-xl font-semibold dark:text-white md:text-xl">
-              ${coin.market_data?.current_price.usd}
-            </p>
+        {/* Coin Stat */}
+        <div className="flex w-full flex-col items-center justify-center gap-2">
+          <div className="flex w-full flex-col items-center justify-center gap-2">
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">Market Değeri </p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.market_cap.usd}
+              </p>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">Hacim(24sa)</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.total_volume.usd.toLocaleString("tr-TR")}
+              </p>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">Dolaşımdaki Arz</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.circulating_supply.toLocaleString("tr-TR")}
+              </p>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">Toplam Arz</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.total_supply.toLocaleString("tr-TR")}
+              </p>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">Maksimum Arz</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.max_supply.toLocaleString("tr-TR")}
+              </p>
+            </div>
           </div>
-          {/* --- */}
-          <div className="flex flex-col items-center justify-center gap-2 px-4 py-2">
-            <p className="rounded-full px-2 py-0.5  text-base font-medium text-blue-500 dark:bg-blue-100 dark:text-slate-900 md:text-base">
-              Market Cap
-            </p>
-            <p className="text-xl font-semibold dark:text-white md:text-xl">
-              ${coin.market_data?.market_cap.usd}
-            </p>
+          <div className="flex w-full flex-col items-center justify-center gap-2">
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">En Yüksek Değer (24h)</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.high_24h.usd.toLocaleString("tr-TR")}
+              </p>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">En Düşük Değer (24h)</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.low_24h.usd.toLocaleString("tr-TR")}
+              </p>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <p className="text-sm font-medium dark:text-white">Değişim (24sa)</p>
+              <p className="text-sm font-medium dark:text-white">
+                {coin.market_data?.price_change_percentage_24h.toLocaleString("tr-TR")}%{" "}
+              </p>
+            </div>
           </div>
-
-          {/* --- */}
-          <div className="flex flex-col items-center justify-center gap-2 px-4 py-2">
-            <p className="rounded-full px-2 py-0.5 text-base font-medium text-blue-500 dark:bg-blue-100 dark:text-slate-900 md:text-base">
-              Total Volume
-            </p>
-            <p className="text-xl font-semibold dark:text-white md:text-xl">
-              ${coin.market_data?.total_volume.usd}
-            </p>
-          </div>
-          {/* --- */}
-          <div className="flex flex-col items-center justify-center gap-2 px-4 py-2">
-            <p className="rounded-full px-2 py-0.5  text-base font-medium text-blue-500 dark:bg-blue-100 dark:text-slate-900 md:text-base">
-              24h High
-            </p>
-            <p className="text-xl font-semibold dark:text-white md:text-xl">
-              ${coin.market_data?.high_24h.usd}
-            </p>
-            {/* --- */}
-          </div>
+        </div>
+        {/* Coin Chart */}
+        <div className="flex w-full items-center justify-center">
+          <Sparklines data={coin.market_data?.sparkline_7d.price}>
+            <SparklinesLine color="#3B82F6" />
+          </Sparklines>
         </div>
       </div>
     </div>
